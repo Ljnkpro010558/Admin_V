546213d24f1a6103edbcb3fc7919b966aae8c7c6
Dead_Max <ljnkpro010558@gmail.com>
2021-11-29 11:27:03 +0700

    a

--- your-app/src/Pages/Info.js
+++ your-app/src/Pages/Info.js
@@ -9,14 +9,12 @@ export default class Info extends Component{
            <div id="wrapper" >
               <Header></Header>
               <div className="container-fluid" style={{ marginTop: '40px'}}>
-                <div className="container mt-2 d-flex justify-content-around" style={{backgroundColor:'whitesmoke'}}>
+                <div className="container mt-3 d-flex justify-content-around" style={{backgroundColor:'whitesmoke'}}>
 
                     <div style={{width: '100%'}}>
-                    <h3 class=" title-comm"><span class="title-holder">ỨNG DỤNG TĂNG CƯỜNG THỰC TẾ</span></h3>
+                    <h3 class=" title-comm"><span class="title-holder">Thông Tin liên hệ</span></h3>
 
-                    <div id='nz-div'>
-	                    <h3 class="tde"><span class="null">THIẾT BỊ SỐ PHỤ KIỆN</span></h3>
-                </div>
+                   
                     </div>
               
                 </div>
